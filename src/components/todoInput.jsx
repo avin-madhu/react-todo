dad198dfd812da4ca1000e85bb96695f46baf8f6
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2024-07-09 11:18:09 +0000

    enter key pressed event

--- src/components/todoInput.jsx
+++ src/components/todoInput.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 export default function TodoInput(props)
 {
     const { handleAddTodos,todoValue, setTodoValue  } = props
-
+    const buttonref = useRef(null)
+    const handleKeyPressed = ((e)=>{
+        if(e.key == 'Enter')
+        {
+            buttonref.current.click()
+            handleAddTodos(todoValue)
+            setTodoValue('')
+        }
+    })
     return(
 
         <header>
             <input value={todoValue} onChange={(e)=>{
                 setTodoValue(e.target.value)
-            }} placeholder="Enter task..." type="text" />
-            <button onClick={()=>{
+            }} placeholder="Enter task..." type="text" onKeyDown={handleKeyPressed} />
+            <button ref={buttonref} onClick={()=>{
                 handleAddTodos(todoValue)
                 setTodoValue('')
             }}>add</button>
